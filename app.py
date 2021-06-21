961f12d9aed5f4dd993298e2b05732d334ab7aa9
Gaetan Manchon <gmanchon@gmail.com>
2021-06-21 09:11:00 +0200

    fill dpt color depending on dpt code

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ import folium
 
 
 def color_department(feat):
-    return "lightblue" if int(feat["properties"]["code"][:1]) < 5 else "red"
+
+    # get dpt code
+    code = feat["properties"]["code"]
+
+    # convert dpt
+    try:
+        code = int(code)
+    except ValueError:
+        code = 0
+
+    # convert dpt to hex
+    return "#0000{:02x}".format(int(code / 95 * 255))
 
 
 def highlight_department(feat):
@@ -21,9 +32,11 @@ folium.GeoJson(
     "geojson.json",
     name="geojson",
     style_function=lambda feat: {
-        "weight": 2,
+        "weight": 1,
         "color": "black",
         "fillColor": color_department(feat),
+        "opacity": 0.25,
+        "fillOpacity": .25,
     },
     highlight_function=lambda feat: {
         "fillColor": highlight_department(feat),
